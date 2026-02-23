aef4d34bf9a5c6974617ae27979de715f3df712e
Kim Tao <kim.tao@mongodb.com>
2026-02-23 08:02:27 -0800

    DEVPROD-25314: no-op setting nice in Windows (#9851)

--- agent/util/subtree.go
+++ agent/util/subtree.go
@@ -1,9 +1,6 @@
 package util
 
 import (
-	"runtime"
-	"syscall"
-
 	"github.com/pkg/errors"
 )
 
@@ -27,18 +24,3 @@ const (
 	// maxNice is the maximum nice value (i.e. lowest priority).
 	maxNice = 19
 )
-
-// SetNice sets the nice for the process given by PID. This determines its
-// relative scheduling priority for host CPU.
-// This is only available if the current process has sufficient permissions to
-// set the nice.
-func SetNice(pid, nice int) error {
-	if runtime.GOOS != "linux" {
-		return nil
-	}
-	if nice < minNice || nice > maxNice {
-		return errors.Errorf("nice must be between %d and %d", minNice, maxNice)
-	}
-	// 0 refers to the current process.
-	return syscall.Setpriority(syscall.PRIO_PROCESS, pid, nice)
-}
